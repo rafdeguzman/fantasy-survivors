8741bd6ecf801ceabdcf7d24cdf70b1948a25b23
Rafael Vincent De Guzman <1824975@johnabbottcollege.net>
2022-12-13 15:36:43 -0500

    progress

--- src/entities/enemy.ts
+++ src/entities/enemy.ts
@@ -13,65 +13,17 @@ export default class Enemy extends GameEntity{
     public enemyBullets: BulletGroup;
 
     constructor(scene: Phaser.Scene, x: number,
-        y: number) {
-        super(scene, x, y, 'orc');
+        y: number, texture: string, frame?: string | number) {
+        super(scene, x, y, texture, frame);
         
         this.scene = scene;
 
-        this.enemyBullets = new BulletGroup(scene);
-
-        this.initSprite();
         this.initPhysics();
-        this.initAnimations();
-    }
-
-    initSprite(): void{
-        this.originY = 0.6;
-        this.body.setCircle(9);
-        this.body.setOffset(0, 3);
-        this.setDisplaySize(72, 112);
     }
 
     initPhysics(): void{
         this.scene.physics.add.existing(this);
         this.scene.physics.world.disable(this);
-    }
-
-    initAnimations(): void{
-        this.scene.anims.create({
-            key: 'orc_run',
-            frames: this.scene.anims.generateFrameNames('orc', {prefix: 'orc_run_', start: 0, end: 3}),
-            frameRate: 10,
-        });
-    }
-
-    // walk towards the player
-    update(time: number, delta: number): void {
-        this.tick += delta
-
-        this.scene.physics.moveToObject(this, this.scene.player, this.SPEED);
-
-        if (this.body.velocity.x > 0) { // walking right, facing rght
-            this.setFlipX(false);
-        } else if (this.body.velocity.x < 0) {  // walking left, facing left
-            this.setFlipX(true);
-        } 
-        
-        this.rotation = -this.scene.cameras.main.rotation;
-
-        !this.anims.isPlaying && this.anims.play('orc_run', true);
-
-        this.handleShooting();
-    }
-
-    spawn(x: number, y: number): void {
-        this.scene.physics.world.enable(this);
-        this.body.reset(x, y);
-
-        this.setActive(true);
-        this.setVisible(true);
-        
-        this.initSprite();
     }
 
     takeDamage(damage: number): void {
@@ -88,17 +40,5 @@ export default class Enemy extends GameEntity{
         this.scene.time.delayedCall(100, () => {
             this.clearTint();
         });
-    }
-
-    handleShooting(): void {
-        if (this.tick > 1500) {
-            this.shoot();
-            this.scene.gunshotSound.play({volume: 0.1});
-            this.tick = 0;
-        }
-    }
-
-    shoot(): void {
-        this.enemyBullets.fireSpreadBullet(this, this.scene.player, 500);
     }
 }
