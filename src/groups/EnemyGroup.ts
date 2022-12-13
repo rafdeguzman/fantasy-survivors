72089b7f926a5f499473526b8e8d89c7d115a11a
Rafael Vincent De Guzman <1824975@johnabbottcollege.net>
2022-12-13 13:38:04 -0500

    encapsulated overlap logic

--- src/groups/EnemyGroup.ts
+++ src/groups/EnemyGroup.ts
@@ -16,20 +16,24 @@ export default class EnemyGroup extends Phaser.Physics.Arcade.Group {
         });
     }
 
-    spawnEnemy(x: number, y: number): void {
-        this.create(x, y, 'enemy', 0, false, false);
-        
-        const enemy = this.getFirstDead(false);
-
+    initOverlaps(enemy: Enemy) : void {
         this.scene.physics.add.overlap(this.scene.player, enemy, () => {
             this.scene.player.takeDamage(GLOBALS.ENEMY_DAMAGE);
-        })
+        });
 
         this.scene.physics.add.overlap(enemy.enemyBullets, this.scene.player, (player: Player, bullet: Bullet) => {
             if (!bullet.active || player.isInvulnerable) return;
             bullet.destroy();
             player.takeDamage(GLOBALS.BULLET_DAMAGE);
-        })
+        });        
+    }
+
+    spawnEnemy(x: number, y: number): void {
+        this.create(x, y, 'enemy', 0, false, false);
+        
+        const enemy = this.getFirstDead(false);
+
+        this.initOverlaps(enemy);
 
         if (enemy) {
             enemy.spawn(x, y);
