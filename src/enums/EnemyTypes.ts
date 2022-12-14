c8c9823fb046445261d3c028e3de1073b30f39fa
Rafael Vincent De Guzman <1824975@johnabbottcollege.net>
2022-12-14 03:01:32 -0500

    created enemytypes enum

---
+++ src/enums/EnemyTypes.ts
@@ -0,0 +1,12 @@
+enum EnemyTypes {
+    BigZombie = 'big_zombie',
+    Ogre = 'ogre',
+    Zombie = 'zombie',
+    TinyZombie = 'tiny_zombie',
+    Necromancer = 'necromancer',
+    Orc = 'orc',
+    Demon = 'demon',
+    Shaman = 'shaman',
+}
+
+export default EnemyTypes;
