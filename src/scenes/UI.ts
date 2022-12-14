2c9034e682417f6e9ced73777cbc8b8cc611d4c7
Gorav-K <78222737+Gorav-K@users.noreply.github.com>
2022-12-13 19:14:39 -0500

    hearth is done

--- src/scenes/UI.ts
+++ src/scenes/UI.ts
@@ -28,28 +28,57 @@ export default class UI extends Phaser.Scene {
     update() {
         this.countDown.update();
         console.log(this.player.health)
-        //healthStatus();
+        this.healthStatus();
     }
     healthStatus() {
+
+        //remove all images
+        this.children.each(function (child) {
+            if (child instanceof Phaser.GameObjects.Image) {
+                child.destroy();
+            }
+        });
+
             switch (this.player.health) {
+                case 6:
+                    this.add.image(150, 70, 'heart');
+                    this.add.image(200, 70, 'heart');
+                    this.add.image(250, 70, 'heart');
+                    break;
                 case 5:
-                    this.add.image(50, 50, 'health');
+                    this.add.image(150, 70, 'heart');
+                    this.add.image(200, 70, 'heart');
+                    this.add.image(250, 70, 'heartHalf');
                     break;
                 case 4:
-                    this.add.image(50, 50, 'health4');
+                    this.add.image(150, 70, 'heart');
+                    this.add.image(200, 70, 'heart');
+                    this.add.image(250, 70, 'heartEmpty');
                     break;
                 case 3:
-                    this.add.image(50, 50, 'health3');
+                    this.add.image(150, 70, 'heart');
+                    this.add.image(200, 70, 'heartHalf');
+                    this.add.image(250, 70, 'heartEmpty');
                     break;
                 case 2:
-                    this.add.image(50, 50, 'health2');
+                    this.add.image(150, 70, 'heart');
+                    this.add.image(200, 70, 'heartEmpty');
+                    this.add.image(250, 70, 'heartEmpty');
                     break;
                 case 1:
-                    this.add.image(50, 50, 'health1');
+                    this.add.image(150, 70, 'heartHalf');
+                    this.add.image(200, 70, 'heartEmpty');
+                    this.add.image(250, 70, 'heartEmpty');
                     break;
-                case 0:
-                    break;
+                default:
+                break;
         }   
+                // scale all images to 1.5 
+                this.children.each(function (child) {
+                    if (child instanceof Phaser.GameObjects.Image) {
+                        child.setScale(1.5);
+                    }
+                });
     }
 
     timetime() {
